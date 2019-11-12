3beddcf4d72cc10086c06421afd31caa650a0f4d
knakano1 <knakano1@swarthmore.edu>
2019-11-12 12:18:35 -0500

    done with project

--- product/models.py
+++ product/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Product class
+class Product(models.Model):
+    # title
+    title = models.CharField(max_length = 255)
+    # url
+    url = models.TextField()
+    # pub_date
+    pub_date = models.DateTimeField()
+    # votes_total
+    votes_total = models.IntegerField(default=1)
+    # image
+    image = models.ImageField(upload_to = 'images/')
+    # icon
+    icon = models.ImageField(upload_to = 'images/')
+    # body
+    body = models.TextField()
+    # hunter (person who submitted or found the product)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # function that will allow the admin page to display each blog title
+    def __str__(self):
+        return self.title
+
+    # function that will return certain portion of the body
+    def summary(self):
+        return self.body[:100]+"..."
+
+    # customize publishing dates
+    # pub_date_pretty
+    def pub_date_neat(self):
+        return self.pub_date.strftime('%B %e, %Y')
--- product_hunt/settings.py
+++ product_hunt/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'product_hunt/static/')
 ]
-#STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
